a37f59adda905f50a228b50ed6c2198bfd55053a
Nimor111 <georgi.bojinov@hotmail.com>
2018-09-30 00:37:23 +0300

    Add functional dependency example

--- hashing/src/Data/Stream.purs
+++ hashing/src/Data/Stream.purs
@@ -6,7 +6,7 @@ import Data.String as String
 import Data.Monoid (class Monoid, mempty)
 import Data.Maybe (Maybe(..))
 
-class Stream stream element where
+class Stream stream element | stream -> element where
   uncons :: stream -> Maybe { head :: element, tail :: stream }
 
 instance streamArray :: Stream (Array a) a where
@@ -20,3 +20,5 @@ foldStream f list =
   case uncons list of
     Nothing -> mempty
     Just cons -> f cons.head <> foldStream f cons.tail
+
+genericTail xs = map _.tail (uncons xs)
